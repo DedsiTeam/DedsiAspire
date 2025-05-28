d9b7fcae9f7a7371bead9d143860eaebcf52607a
Cohen <1768065921@qq.com>
2025-05-28 17:33:25 +0800

    DedsiAuthCenter.Host add AuthApis

---
+++ DedsiAuthCenter/DedsiAuthCenter.Host/Apis/AuthApis.cs
@@ -0,0 +1,11 @@
+﻿namespace DedsiAuthCenter.Host.Apis;
+
+public static class AuthApis
+{
+    public static RouteGroupBuilder MapAuthApis(this IEndpointRouteBuilder app)
+    {
+        var api = app.MapGroup("api/auth");
+
+        return api;
+    }
+}
--- DedsiAuthCenter/DedsiAuthCenter.Host/Program.cs
+++ DedsiAuthCenter/DedsiAuthCenter.Host/Program.cs
@@ -1,3 +1,5 @@
+using DedsiAuthCenter.Host.Apis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -12,5 +14,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapAuthApis();
 
 app.Run();
