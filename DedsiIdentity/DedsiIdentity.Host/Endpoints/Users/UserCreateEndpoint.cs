2130ad1942e12a92e78d5b77a72769d31fb7cc5e
Cohen <1768065921@qq.com>
2025-05-29 10:27:04 +0800

    add files
    add files

---
+++ DedsiIdentity/DedsiIdentity.Host/Endpoints/Users/UserCreateEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+
+namespace DedsiIdentity.Host.Endpoints.Users;
+
+public record UserCreateRequest(string Name, string Email, string Password);
+
+public record UserCreateResponse(Guid Id);
+
+
+public class UserCreateEndpoint : Endpoint<UserCreateRequest, UserCreateResponse>
+{
+    public override void Configure()
+    {
+        Post("/api/user/create");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(UserCreateRequest request, CancellationToken ct)
+    {
+        await SendAsync(new UserCreateResponse(Guid.NewGuid()));
+    }
+}
